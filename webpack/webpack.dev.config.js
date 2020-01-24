05c1ddd3a9f625731551c81a5c275d5fb449200e
lakshmig <lakshmig@thoughtworks.com>
2020-01-24 11:49:29 +0530

    <EKA-19> Add proxy as part of webpack config

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -10,12 +10,34 @@ module.exports = {
   plugins: [
     ...commonWebpackConfig.plugins,
     new webpack.DefinePlugin({
-      BACKEND_BASE_URL: JSON.stringify("http://localhost:3000")
+      BACKEND_BASE_URL: JSON.stringify("http://localhost:8080/")
     })
   ],
   devServer: {
     contentBase: parentDir,
-    historyApiFallback: true
+    historyApiFallback: true,
+    proxy: {
+      "/patients/*": {
+        changeOrigin: true,
+        cookieDomainRewrite: "http://hiu-dev.projecteka.in",
+        target: "http://hiu-dev.projecteka.in",
+        onProxyReq: proxyReq => {
+          if (proxyReq.getHeader("origin")) {
+            proxyReq.setHeader("origin", "http://hiu-dev.projecteka.in");
+          }
+        }
+      },
+      "/consent-requests": {
+        changeOrigin: true,
+        cookieDomainRewrite: "http://hiu-dev.projecteka.in",
+        target: "http://hiu-dev.projecteka.in",
+        onProxyReq: proxyReq => {
+          if (proxyReq.getHeader("origin")) {
+            proxyReq.setHeader("origin", "http://hiu-dev.projecteka.in");
+          }
+        }
+      }
+    }
   },
   devtool: "eval-source-map"
 };
