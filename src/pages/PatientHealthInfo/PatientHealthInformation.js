4231e29c88e8a9414113d8b3c265ded182289e2b
lakshmig <lakshmig@thoughtworks.com>
2020-03-11 12:11:38 +0530

    <EKA-102> Group entries by day and hip WIP

--- src/pages/PatientHealthInfo/PatientHealthInformation.js
+++ src/pages/PatientHealthInfo/PatientHealthInformation.js
@@ -4,8 +4,41 @@ import HealthInfoNav from "../../components/HealthInfoNav/HealthInfoNav";
 import HealthInfoContainer from "../../components/HealthInfoContainer/HealthInfoContainer";
 import { useParams } from "react-router-dom";
 
+function filterByHipId(entriesByHips, hipId) {
+  return entriesByHips.filter(e => {
+    return e.hipId === hipId;
+  });
+}
+
+function groupByHip(entries) {
+  var entriesByHips = [];
+  entries.forEach(entry => {
+    var entryByHip;
+    var hipEntries = filterByHipId(entriesByHips, entry.hipId);
+    if (hipEntries && hipEntries.length > 0) {
+      entryByHip = hipEntries[0];
+    }
+    if (entryByHip == undefined) {
+      entryByHip = { hipId: entry.hipId, hipName: entry.hipName, bundles: [] };
+      entriesByHips.push(entryByHip);
+      if (entry.data) {
+        entryByHip.bundles.push(entry.data);
+      }
+    } else {
+      entryByHip.bundles.push(entry.data);
+    }
+  });
+  return entriesByHips;
+}
+
+function groupByDay(entries) {
+  var entriesByDate = [];
+}
+
 function groupByDate(data) {
-  console.log("***data", data);
+  var entriesByHips = groupByHip(data.data.entries);
+  groupByDay(entriesByHips);
+
   return data;
 }
 
