21ad46e0a037255824a135ee6192fe0ddfed81c4
lakshmig <lakshmig@thoughtworks.com>
2020-03-11 15:27:13 +0530

    <EKA-102> component updated: date navigator to change date selected

---
+++ src/components/HealthInfoNav/HealthInfoNav.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const HealthInfoNavStyles = styled.div`
+  .health-info-nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 0;
+  }
+  .date-navigator {
+    display: flex;
+    align-items: center;
+  }
+  .date-navigator-heading {
+    padding: 10px 0 2px 0;
+  }
+  .date-text {
+    padding: 0 10px;
+  }
+`;
+
+export default HealthInfoNavStyles;
