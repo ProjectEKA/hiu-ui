903b8be3fbcbe6ff65cf5f531c46405f33852ad2
lakshmig <lakshmig@thoughtworks.com>
2020-03-11 11:00:45 +0530

    <EKA-107> Transform health data to map to tables on observation and diagnostic report components

--- src/components/DiagnosticReport/DiagnosticReportTable.js
+++ src/components/DiagnosticReport/DiagnosticReportTable.js
@@ -8,130 +8,49 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import DiagnosticReportTableStyles from "./DiagnosticReportTable.style";
-import {
-  generateObservableEntity,
-  generateObservableEntityValue,
-  generateObservableEntityStatusInterpretation,
-  generateRowsForResults
-} from "./DiagnosticReportTableHelperFunctions";
-import IconButton from "@material-ui/core/IconButton";
-import { ArrowDropDown, ArrowRight } from "@material-ui/icons";
-import { toIndiaDate } from "../../constants";
-
-const DiagnosticResults = ({ results, close }) => {
-  return results
-    ? results.map((result, i) => (
-        <TableRow
-          className={close ? "close" : ""}
-          style={{ backgroundColor: "primary" }}
-          key={i}
-        >
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell>
-            {generateObservableEntityStatusInterpretation(result)}
-          </TableCell>
-          <TableCell>{generateObservableEntity(result)}</TableCell>
-          <TableCell>{generateObservableEntityValue(result)}</TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      ))
-    : null;
-};
+import Typography from "@material-ui/core/Typography";
 
 const DiagnosticReportTable = ({
   loadHealthData,
   healthInfo,
-  downloadPathology,
   consentRequestId
 }) => {
-  const [close, setClose] = useState(0);
   useEffect(() => {
     loadHealthData(consentRequestId);
   }, []);
-
-  console.log("&&&&&&", consentRequestId);
-
-  function extractDiagnosticReport(healthInfo) {
-    if (healthInfo && healthInfo.entries) {
-      const ObservationData = healthInfo.entries[0].data;
-      if (ObservationData) {
-        const DiagnosticReport = ObservationData.entry.filter(
-          item =>
-            item.resource.resourceType === "DiagnosticReport" ||
-            item.resource.resourceType === "Observation"
-        );
-        return DiagnosticReport;
-      }
-    }
-    return undefined;
-  }
-  const DiagnosticReport = extractDiagnosticReport(healthInfo);
-  const Results = DiagnosticReport && generateRowsForResults(DiagnosticReport);
-
-  console.log("*Results*", Results);
-
-  return DiagnosticReport ? (
+  return healthInfo ? (
     <DiagnosticReportTableStyles>
-      <TableContainer component={Paper}>
-        <Table className="observation-table" aria-label="simple table">
+      <Typography className="header" gutterBottom variant="h6" component="h2">
+        {healthInfo.DiagnosticReport.resource_type}
+      </Typography>
+      <TableContainer
+        className="diagnostic-report-table-container"
+        component={Paper}
+      >
+        <Table className="diagnostic-report-table" aria-label="simple table">
           <TableHead>
             <TableRow className="table-head">
-              <TableCell></TableCell>
               <TableCell align="left">Report Date</TableCell>
-              <TableCell align="left">Status of the report</TableCell>
               <TableCell align="left">Report</TableCell>
               <TableCell align="left">Result</TableCell>
               <TableCell align="left">Performer</TableCell>
+              <TableCell align="left">Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                {Results ? (
-                  close ? (
-                    <IconButton
-                      className="expand-button"
-                      size="small"
-                      onClick={() => setClose(!close)}
-                      color="primary"
-                    >
-                      <ArrowRight />
-                    </IconButton>
-                  ) : (
-                    <IconButton
-                      className="expand-button"
-                      size="small"
-                      onClick={() => setClose(!close)}
-                      color="primary"
-                    >
-                      <ArrowDropDown />
-                    </IconButton>
-                  )
-                ) : null}
-              </TableCell>
-              <TableCell align="left">
-                {toIndiaDate(DiagnosticReport[0].resource.effectiveDateTime)}
-              </TableCell>
-              <TableCell align="left">
-                {generateObservableEntityStatusInterpretation(
-                  DiagnosticReport[0].resource
-                )}
-              </TableCell>
-              <TableCell align="left">
-                {generateObservableEntityValue(DiagnosticReport[0].resource)}
-              </TableCell>
-              <TableCell
-                align="left"
-                onClick={() => downloadPathology(consentRequestId)}
-              >
-                Reportname_Link
-              </TableCell>
-              <TableCell align="left">
-                {generateObservableEntity(DiagnosticReport[0].resource)}
-              </TableCell>
-            </TableRow>
-            <DiagnosticResults results={Results} close={close} />
+            {healthInfo.DiagnosticReport.data.map((row, i) => (
+              <TableRow>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.observation}</TableCell>
+                <TableCell>{}</TableCell>
+                <TableCell>{row.performer}</TableCell>
+                <TableCell>
+                  {row.status_and_interpretation.status}
+                  {row.status_and_interpretation.delimiter}
+                  {row.status_and_interpretation.interpretation}
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -151,4 +70,5 @@ const DiagnosticReportTable = ({
     </DiagnosticReportTableStyles>
   );
 };
+
 export default DiagnosticReportTable;
