42b80f7e9d55899549566725514204f6577ec768
Angshuman Sarkar <angshuonline@gmail.com>
2020-03-16 11:04:13 +0530

    EKA-107 | Introducing Daywise data group capability

--- src/pages/PatientHealthInfo/PatientHealthInformation.js
+++ src/pages/PatientHealthInfo/PatientHealthInformation.js
@@ -3,44 +3,7 @@ import PatientDetails from "../../components/PatientDetails/PatientDetails";
 import HealthInfoNav from "../../components/HealthInfoNav/HealthInfoNav";
 import HealthInfoContainer from "../../components/HealthInfoContainer/HealthInfoContainer";
 import { useParams } from "react-router-dom";
-
-function filterByHipId(entriesByHips, hipId) {
-  return entriesByHips.filter(e => {
-    return e.hipId === hipId;
-  });
-}
-
-function groupByHip(entries) {
-  var entriesByHips = [];
-  entries.forEach(entry => {
-    var entryByHip;
-    var hipEntries = filterByHipId(entriesByHips, entry.hipId);
-    if (hipEntries && hipEntries.length > 0) {
-      entryByHip = hipEntries[0];
-    }
-    if (entryByHip == undefined) {
-      entryByHip = { hipId: entry.hipId, hipName: entry.hipName, bundles: [] };
-      entriesByHips.push(entryByHip);
-      if (entry.data) {
-        entryByHip.bundles.push(entry.data);
-      }
-    } else {
-      entryByHip.bundles.push(entry.data);
-    }
-  });
-  return entriesByHips;
-}
-
-function groupByDay(entries) {
-  var entriesByDate = [];
-}
-
-function groupByDate(data) {
-  var entriesByHips = groupByHip(data.data.entries);
-  groupByDay(entriesByHips);
-
-  return data;
-}
+import dayGrouper from "../../components/common/HealthInfo/DaywiseGroup";
 
 const patient = {
   name: "john",
@@ -59,7 +22,7 @@ const selectedDate = "12/01/2000";
 
 const PatientHealthInformation = ({ loadHealthData, match }) => {
   useEffect(() => {
-    loadHealthData({ id: match.params.requestId, groupFunction: groupByDate });
+    loadHealthData({ id: match.params.requestId, groupFunction: dayGrouper });
   }, []);
   return (
     <div>
