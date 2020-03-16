b0742d9a4ab303f17bafb43e9fda380450086c62
lakshmig <lakshmig@thoughtworks.com>
2020-03-16 21:46:12 +0530

    <EKA-102> generate data to render health info tables [WIP]

--- src/components/DiagnosticReport/DiagnosticReportTable.js
+++ src/components/DiagnosticReport/DiagnosticReportTable.js
@@ -9,49 +9,45 @@ import Paper from "@material-ui/core/Paper";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import DiagnosticReportTableStyles from "./DiagnosticReportTable.style";
 import Typography from "@material-ui/core/Typography";
+import { toIndiaDate } from "../../constants";
+import getNestedObject from "./../../utils/getNestedObject";
 
-const DiagnosticReportTable = ({
-  loadHealthData,
-  healthInfo,
-  consentRequestId
-}) => {
-  useEffect(() => {
-    loadHealthData(consentRequestId);
-  }, []);
-  return healthInfo ? (
+const DiagnosticReportTable = ({ hipName, data }) => {
+  return hipName && data ? (
     <DiagnosticReportTableStyles>
       <Typography className="header" gutterBottom variant="h6" component="h2">
-        {healthInfo.DiagnosticReport.resource_type}
+        {hipName}
       </Typography>
-      <TableContainer
-        className="diagnostic-report-table-container"
-        component={Paper}
-      >
-        <Table className="diagnostic-report-table" aria-label="simple table">
+      <TableContainer component={Paper}>
+        <Table className="observation-table" aria-label="simple table">
           <TableHead>
             <TableRow className="table-head">
               <TableCell align="left">Report Date</TableCell>
+              <TableCell align="left">Status of the report</TableCell>
               <TableCell align="left">Report</TableCell>
               <TableCell align="left">Result</TableCell>
               <TableCell align="left">Performer</TableCell>
-              <TableCell align="left">Status</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {healthInfo.DiagnosticReport.data.map((row, i) => (
-              <TableRow>
-                <TableCell>{row.date}</TableCell>
-                <TableCell>{row.observation}</TableCell>
-                <TableCell>{}</TableCell>
-                <TableCell>{row.performer}</TableCell>
-                <TableCell>
-                  {row.status_and_interpretation.status}
-                  {row.status_and_interpretation.delimiter}
-                  {row.status_and_interpretation.interpretation}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+          {data.map(entry => {
+            return (
+              <TableBody>
+                <TableRow>
+                  <TableCell align="left">
+                    {entry.effectiveDateTime
+                      ? toIndiaDate(entry.effectiveDateTime)
+                      : "-"}
+                  </TableCell>
+                  <TableCell align="left">{entry.status}</TableCell>
+                  <TableCell align="left">
+                    {entry.code ? entry.code.text : ""}
+                  </TableCell>
+                  <TableCell align="left"></TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+              </TableBody>
+            );
+          })}
         </Table>
       </TableContainer>
     </DiagnosticReportTableStyles>
