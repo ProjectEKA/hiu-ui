5c211846fed97ea274c60bb7fb4ad4fcf8e7ad82
lakshmig <lakshmig@thoughtworks.com>
2020-03-27 13:00:07 +0530

    <EKA-000> Add material table module to enable sorting and filtering features

--- src/components/ConsentsListTable/ConsentsListTable.js
+++ src/components/ConsentsListTable/ConsentsListTable.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Styles from "../../constants/tableConstants";
 import { formatDateString } from "../common/HealthInfo/FhirResourcesUtils";
+import MaterialTable from "material-table";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-const ConsentsListTable = ({ loadConsents, consentsList }) => {
+const useStyles = makeStyles(theme => ({
+  table: {
+    clear: "both",
+    backgroundColor: theme.palette.primary.main
+  },
+  title: {
+    textTransform: "uppercase"
+  }
+}));
+
+const ConsentsListTable = ({ loadConsents, consentsList, theme }) => {
+  const classes = useStyles();
   useEffect(() => {
     loadConsents();
   }, []);
@@ -33,57 +39,50 @@ const ConsentsListTable = ({ loadConsents, consentsList }) => {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={Styles().table} aria-label="simple table">
-        <TableHead>
-          {
-            <TableRow className={Styles().tableHead}>
-              <TableCell>{headerRow.name}</TableCell>
-              <TableCell>{headerRow.jataayuId}</TableCell>
-              <TableCell>{headerRow.requestStatus}</TableCell>
-              <TableCell>{headerRow.consentGrantedDate}</TableCell>
-              <TableCell>{headerRow.consentExpiryDate}</TableCell>
-              <TableCell>{""}</TableCell>
-            </TableRow>
+    <div style={{ maxWidth: "100%" }}>
+      <MaterialTable
+        options={{
+          headerStyle: {
+            backgroundColor: "#009688",
+            color: "#FFF"
           }
-        </TableHead>
-        <TableBody>
-          {consentsList.map((consent, i) => (
-            <TableRow
-              // className={i % 2 !== 0 ? Styles().evenTableRow : ""}
-              key={consent.id}
-            >
-              <TableCell>{getPatientFullName(consent.patient)}</TableCell>
-              <TableCell>{consent.patient.id}</TableCell>
-              <TableCell>
-                {isGrantedConsent(consent.status)
-                  ? "Consent granted"
-                  : "Request sent"}
-              </TableCell>
-              <TableCell>
-                {isGrantedConsent(consent.status)
-                  ? formatDateString(consent.approvedDate)
-                  : "-"}
-              </TableCell>
-              <TableCell>
-                {isGrantedConsent(consent.status)
-                  ? formatDateString(consent.expiredDate)
-                  : "-"}
-              </TableCell>
-              <TableCell>
-                {isGrantedConsent(consent.status) ? (
-                  <Link to={`/health-info/${consent.id}`}>
-                    <ArrowForwardIosIcon color="primary" />
-                  </Link>
-                ) : (
-                  ""
-                )}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        }}
+        className={classes.table}
+        columns={[
+          { title: headerRow.name, field: "name" },
+          { title: headerRow.jataayuId, field: "id" },
+          { title: headerRow.requestStatus, field: "status" },
+          { title: headerRow.consentGrantedDate, field: "grantedOn" },
+          { title: headerRow.consentExpiryDate, field: "expiredOn" },
+          { title: "", field: "navLink", width: 50 }
+        ]}
+        data={consentsList.map(consent => ({
+          name: getPatientFullName(consent.patient),
+          id: consent.patient.id,
+          status: isGrantedConsent(consent.status)
+            ? "Consent granted"
+            : "Request sent",
+          grantedOn: isGrantedConsent(consent.status)
+            ? formatDateString(consent.approvedDate)
+            : "-",
+          expiredOn: isGrantedConsent(consent.status)
+            ? formatDateString(consent.expiredDate)
+            : "-",
+          navLink: isGrantedConsent(consent.status) ? (
+            <Link to={`/health-info/${consent.id}`}>
+              <ArrowForwardIosIcon color="primary" />
+            </Link>
+          ) : (
+            ""
+          )
+        }))}
+        title={
+          <Typography className={classes.title} variant="h5">
+            Consent List
+          </Typography>
+        }
+      />
+    </div>
   );
 };
 
