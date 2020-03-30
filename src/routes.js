a1e770c0d9671f90fafc185ad4479fb393abd420
lakshmig <lakshmig@thoughtworks.com>
2020-03-30 13:39:48 +0530

    <EKA-172> Create login template

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import RequestAccess from "./pages/RequestAccess/RequestAccessContainer";
 import LandingPage from "./pages/LandingPage/LandingPageContainer";
 import ConsentLog from "./components/ConsentsListTable/LoadConsentsContainer";
 import PatientHealthInformation from "./pages/PatientHealthInfo/PatientHealthInformationContainer";
+import Login from "./pages/LoginPage/LoginPage";
 const routes = [
   {
     component: LandingPage,
@@ -11,6 +12,11 @@ const routes = [
   {
     component: PatientHealthInformation,
     path: "/health-info/:requestId",
+    isExact: false
+  },
+  {
+    component: Login,
+    path: "/login",
     isExact: false
   }
 ];
