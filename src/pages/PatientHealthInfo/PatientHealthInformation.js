f485dc3a46d6e522641767d514f2a96b5615b470
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-04-13 13:30:30 +0530

    <HIU-14> Show message when no health data is available
    - Bump up @material-ui/core and @material-ui/lab

--- src/pages/PatientHealthInfo/PatientHealthInformation.js
+++ src/pages/PatientHealthInfo/PatientHealthInformation.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import { Alert, AlertTitle } from "@material-ui/lab";
 import PatientDetails from "../../components/PatientDetails/PatientDetails";
 import HealthInfoNav from "../../components/HealthInfoNav/HealthInfoNav";
 import HealthInfoContainer from "../../components/HealthInfoContainer/HealthInfoContainer";
@@ -14,6 +16,7 @@ const PatientHealthInformation = ({
   match
 }) => {
   const [selectedDate, setSelectedDate] = useState(defaultSelectedDate);
+  const isHealthInfoAvailable = !_.isEmpty(healthInfo);
 
   useEffect(() => {
     loadHealthData({ id: match.params.requestId, groupFunction: dayGrouper });
@@ -26,19 +29,29 @@ const PatientHealthInformation = ({
   function onChange(newSelectedDate) {
     setSelectedDate(newSelectedDate);
   }
+  
   return (
     <div>
       <PatientDetails {...patientData} />
-      <HealthInfoNav
-        dates={dateArray}
-        selectedDate={selectedDate}
-        setSelectedDate={onChange}
-      />
-      <HealthInfoContainer
-        consentReqId={match.params.requestId}
-        healthInfo={healthInfo}
-        selectedDate={selectedDate}
-      />
+      {isHealthInfoAvailable ? (
+        <div>
+          <HealthInfoNav
+            dates={dateArray}
+            selectedDate={selectedDate}
+            setSelectedDate={onChange}
+          />
+          <HealthInfoContainer
+            consentReqId={match.params.requestId}
+            healthInfo={healthInfo}
+            selectedDate={selectedDate}
+          />
+        </div>
+      ) : (
+        <Alert severity="info">
+          <AlertTitle><strong>No Information</strong></AlertTitle>
+          Health information is unavailable for requested patient!
+        </Alert>
+      )}
     </div>
   );
 };
