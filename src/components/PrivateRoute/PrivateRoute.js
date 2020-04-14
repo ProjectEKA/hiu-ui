0b92b6533eabb94dd001dd9eac60696894ef4fb0
lakshmig <lakshmig@thoughtworks.com>
2020-04-14 16:15:52 +0530

    <EKA_172> Fix logout part

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
-import getNestedObject from "../../utils/getNestedObject";
 
-const privateRoute = ({ success, component: Component, ...rest }) => {
+const privateRoute = ({ component: Component, ...rest }) => {
   const isAuth = localStorage.getItem("auth-token");
   return (
     <Route
       {...rest}
       render={(props) =>
-        success || isAuth ? <Component {...props} /> : <Redirect to="/login" />
+        isAuth ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
 };
 
-const mapStateToProps = (state) => ({
-  success: getNestedObject(state, "signIn.success"),
-});
-
-export default connect(mapStateToProps)(privateRoute);
+export default privateRoute;
