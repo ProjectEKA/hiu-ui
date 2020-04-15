3c1ae3dfb9ef5a76d4eab3765402d532e96633f1
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-04-15 11:47:03 +0530

    <HIU-14> Show error message for network failure

--- src/pages/PatientHealthInfo/PatientHealthInformation.js
+++ src/pages/PatientHealthInfo/PatientHealthInformation.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import _ from "lodash";
-import { Alert, AlertTitle } from "@material-ui/lab";
 import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 import PatientDetails from "../../components/PatientDetails/PatientDetails";
 import HealthInfoNav from "../../components/HealthInfoNav/HealthInfoNav";
 import HealthInfoContainer from "../../components/HealthInfoContainer/HealthInfoContainer";
 import dayGrouper from "../../components/common/HealthInfo/DaywiseGroup";
+import AlertBox from "../../components/AlertBox/AlertBox";
 
 const PatientHealthInformation = ({
   loadHealthData,
+  error,
+  success,
   dateArray,
   defaultSelectedDate,
   healthInfo,
@@ -35,11 +38,57 @@ const PatientHealthInformation = ({
   };
   const handleSnackbarClose = () => setSnackbarOpen(false);
 
+  let content = (
+    <AlertBox
+      type="info"
+      title="Loading..."
+      message="Fetching data. Please wait!"
+    />
+  );
+
+  if (isHealthInfoAvailable) {
+    content = (
+      <>
+        <PatientDetails {...patientData} />
+        <HealthInfoNav
+          dates={dateArray}
+          selectedDate={selectedDate}
+          setSelectedDate={onChange}
+        />
+        <HealthInfoContainer
+          consentReqId={match.params.requestId}
+          healthInfo={healthInfo}
+          selectedDate={selectedDate}
+        />
+      </>
+    );
+  } else if (error) {
+    content = (
+      <AlertBox
+        type="error"
+        title="Error"
+        message="Sorry! Couldn't fetch data."
+      />
+    );
+  } else if (success && !isHealthInfoAvailable) {
+    content = (
+      <>
+        <PatientDetails {...patientData} />
+        <AlertBox
+          type="info"
+          title="No Information"
+          message="Health information is unavailable for requested patient!"
+        />
+      </>
+    );
+  }
+
   return (
     <div>
       <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
         open={snackbarOpen}
-        autoHideDuration={6000}
+        autoHideDuration={4000}
         onClose={handleSnackbarClose}
       >
         <Alert
@@ -48,32 +97,11 @@ const PatientHealthInformation = ({
           elevation={6}
           variant="filled"
         >
-          Failed to receive data from {erroredEntiresCount} Health Information
-          Providers!
+          Error in the data recieved from {erroredEntiresCount} Health
+          Information Providers!
         </Alert>
       </Snackbar>
-      <PatientDetails {...patientData} />
-      {isHealthInfoAvailable ? (
-        <div>
-          <HealthInfoNav
-            dates={dateArray}
-            selectedDate={selectedDate}
-            setSelectedDate={onChange}
-          />
-          <HealthInfoContainer
-            consentReqId={match.params.requestId}
-            healthInfo={healthInfo}
-            selectedDate={selectedDate}
-          />
-        </div>
-      ) : (
-        <Alert severity="info">
-          <AlertTitle>
-            <strong>No Information</strong>
-          </AlertTitle>
-          Health information is unavailable for requested patient!
-        </Alert>
-      )}
+      {content}
     </div>
   );
 };
