4102f0b8f262e8ea222dc891a2da443fa5e3289b
Sumit Jadhav <sumitsj@thoughtworks.com>
2020-04-24 01:30:14 +0530

    [REFACTOR] Configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+  ],
+  globals: {
+    BASE_NAME: 'readonly',
+    BACKEND_BASE_URL: 'readonly',
+    BACKEND_API_PATH: 'readonly',
+    DICOM_BASE_URL: 'readonly',
+    DICOM_SERVER_PATH: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/setupTests.js', '**/*.test.js']}],
+  },
+};
