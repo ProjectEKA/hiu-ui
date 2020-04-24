798cf26a97b214aeb6bb7c7b75d4ce23124aa1c3
Sumit Jadhav <sumitsj@thoughtworks.com>
2020-04-25 01:07:34 +0530

    [REFACTOR] Fixed eslint issues of Patient Details and Private Route Components

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import _ from 'lodash';
+import * as PropTypes from 'prop-types';
 import jwtDecode from 'jwt-decode';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
 const RESET_PASSWORD_PATH = '/reset-password';
 
-const privateRoute = ({ component: Component, history, ...rest }) => {
+const PrivateRoute = ({ component: Component, history, ...rest }) => {
   const accessToken = localStorage.getItem('auth-token');
   const isAuth = !_.isEmpty(accessToken);
 
@@ -16,7 +17,7 @@ const privateRoute = ({ component: Component, history, ...rest }) => {
     const decodedToken = jwtDecode(accessToken);
     const { isVerified } = decodedToken;
     if (!isVerified) {
-      return rest.path == RESET_PASSWORD_PATH ? (
+      return rest.path === RESET_PASSWORD_PATH ? (
         <Component {...props} />
       ) : (
         <Redirect to={RESET_PASSWORD_PATH} />
@@ -28,4 +29,13 @@ const privateRoute = ({ component: Component, history, ...rest }) => {
   return <Route history={history} {...rest} render={render} />;
 };
 
-export default withRouter(privateRoute);
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+  history: PropTypes.shape({
+    go: PropTypes.func,
+    goBack: PropTypes.func,
+    goForward: PropTypes.func,
+  }).isRequired,
+};
+
+export default withRouter(PrivateRoute);
