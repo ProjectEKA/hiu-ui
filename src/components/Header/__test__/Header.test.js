af881484588904f6cc5977a595ea4cc3d6575261
Sumit Jadhav <sumitsj@thoughtworks.com>
2020-04-25 20:45:46 +0530

    Added unit tests for Header component

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,11 +1,48 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { shallow } from 'enzyme';
+import { Redirect } from 'react-router-dom';
 import Header from '..';
+import { LogoutButton } from '../Header.style';
 
-describe('Header', () => {
-  it('should render properly ', () => {
-    global.BASE_NAME = '/';
-    const wrapper = shallow(<Header />);
-    expect(wrapper.debug()).toMatchSnapshot();
+describe('<Header />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Header />);
+    localStorage.removeItem.mockReset();
+  });
+
+  describe('HTML Structure', () => {
+    it('should render properly', () => {
+      expect(wrapper.debug()).toMatchSnapshot();
+    });
+
+    it('should not render Redirect component when isLoggedOut is false', () => {
+      expect(wrapper.find(Redirect)).toHaveLength(0);
+    });
+
+    it('should render Redirect component when isLoggedOut is true', () => {
+      wrapper.find(LogoutButton).simulate('click');
+      expect(wrapper.find(Redirect)).toHaveLength(1);
+    });
+  });
+
+  describe('Events', () => {
+    describe('logout()', () => {
+      it('should call removeItem() with auth-token', () => {
+        wrapper.find(LogoutButton).simulate('click');
+        expect(localStorage.removeItem).toHaveBeenCalledWith('auth-token');
+      });
+
+      it('should call setState with true', () => {
+        const setState = jest.fn();
+        jest.spyOn(React, 'useState').mockImplementation((init) => [init, setState]);
+        wrapper = shallow(<Header />);
+
+        wrapper.find(LogoutButton).simulate('click');
+
+        expect(setState).toHaveBeenCalledWith(true);
+      });
+    });
   });
 });
